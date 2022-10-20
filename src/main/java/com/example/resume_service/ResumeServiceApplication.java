d9533586181348423e8802995b49faa873e59e3e
orginal <ogabeksodiqov611@gmail.com>
2022-10-20 18:17:27 +0500

    Resume service

---
+++ src/main/java/com/example/resume_service/ResumeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.resume_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run ( ResumeServiceApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/example/resume_service/ResumeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resume_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
