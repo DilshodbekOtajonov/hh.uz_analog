1afe849a6b1dad2f9ce71c208e6e9656ddd91195
Ogabek Sodiqov <ogabeksodiqov611@gmail.com>
2022-10-24 22:05:42 +0500

    Delete src/main/java/com/example/resume_service directory

--- src/main/java/com/example/resume_service/controller/ExperienceController.java
+++
@@ -1,9 +0,0 @@
-package com.example.resume_service.controller;
-
-/**
- * @author "Sodiqov Ogabek"
- * @since 10/21/2022 7:17 PM (Friday)
- * Resume_service/IntelliJ IDEA
- */
-public class ExperienceController {
-}
